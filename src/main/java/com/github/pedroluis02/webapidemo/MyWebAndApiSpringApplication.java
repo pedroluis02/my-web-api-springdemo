0df5085a82e780c3feda26fb7cd35b19c7ba72fa
Pedro Luis <pedroluisnr22@gmail.com>
2023-08-04 23:33:01 -0500

    setup project

---
+++ src/main/java/com/github/pedroluis02/webapidemo/MyWebAndApiSpringApplication.java
@@ -0,0 +1,11 @@
+package com.github.pedroluis02.webapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAndApiSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAndApiSpringApplication.class, args);
+	}
+}
