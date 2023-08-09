35883bfb72fd5fe37d5ced18835a3624515e8168
Pedro Luis <pedroluisnr22@gmail.com>
2023-08-08 21:54:25 -0500

    feat(test): update home controller reference

--- src/test/java/com/github/pedroluis02/webapidemo/web/MyWebAndApiSpringApplicationTests.java
+++ src/test/java/com/github/pedroluis02/webapidemo/web/MyWebAndApiSpringApplicationTests.java
@@ -1,5 +1,6 @@
 package com.github.pedroluis02.webapidemo.web;
 
+import com.github.pedroluis02.webapidemo.web.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
